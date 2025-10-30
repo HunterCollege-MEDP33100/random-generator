57af0815ede59142803d95fc53d5fbd0963dab52
Ivy <ivyzhao335@gmail.com>
2025-10-30 15:05:20 -0400

    update

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    const randomItem = 'this should be a random item';
-  res.render('index', {
-    title: 'My Random Generator',
-    randomItem: randomItem,
-    
- });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
