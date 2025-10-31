f034aecedd6476b58d0613fd3b047bfa07e1feed
Dora Do <doradocodes@gmail.com>
2025-10-31 15:34:02 -0700

    Add fs read for random item in home route
    Replaces hardcoded random item with logic to read from data.json in the home page route. Prepares for selecting a random item from the file and rendering it in the index template.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
+var fs = require('fs');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    const randomItem = 'this should be a random item';
-  res.render('index', {
-    title: 'My Random Generator',
-    randomItem: randomItem,
-    
- });
+router.get('/', function (req, res, next) {
+    let randomItem = '';
+
+    // read from data.json
+    fs.readFile('./data/data.json', 'utf8', function (err, data) {
+        // randomly select an item from data
+
+        // render the index.hbs template with the randomly selected item 
+        res.render('index', {
+            title: 'My Random Generator',
+            randomItem: randomItem,
+        });
+    });
 });
 
 module.exports = router;
