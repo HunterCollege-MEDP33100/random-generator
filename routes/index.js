6015eb89ce976b9fcc35b939c12c0106818b4be8
Ivy <ivyzhao335@gmail.com>
2025-11-02 13:11:53 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-
-});
+  fs.readFile('./data/data.json', 'utf8', function (err, data) {
+    if (err) {
+      console.log(err)
+      res.status(404).send('Not found')
+    } else {
+      const parsedData = JSON.parse(data)
+      const fortunes = data.fortunes
+      const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+      res.render('index', { randomFortune });
+    }
+  })
+}
+);
 
 module.exports = router;
