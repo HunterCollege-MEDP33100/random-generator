a0d80030468a4c9ab60011ef96288034944b6b5a
isabell-lee08 <isabellleee8@gmail.com>
2025-11-03 01:30:36 -0500

    card style / json data

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    let randomItem = '';
+    let randomItemHex = '';
+    let randomItemCode = '';
+    let randomItemName = '';
 
     // read from data.json
     fs.readFile('./data/data.json', 'utf8', function (err, data) {
@@ -17,13 +19,18 @@ router.get('/', function (req, res, next) {
         }
 
         let parseData = JSON.parse(data);
-        let randomNum = Math.floor(Math.random() * 5)
-        let randomItem = parseData[randomNum].hex;
+        let randomNum = Math.floor(Math.random() * parseData.length)
+
+        let randomItemHex = parseData[randomNum].hex;
+        let randomItemCode = parseData[randomNum].code;
+        let randomItemName = parseData[randomNum].name;
 
         // render the index.hbs template with the randomly selected item 
         res.render('index', {
-            title: 'My Random Generator',
-            randomItem: randomItem,
+            title: 'Random Pantone Card',
+            randomItemHex: randomItemHex,
+            randomItemCode: randomItemCode,
+            randomItemName: randomItemName,
         });
     });
 });
