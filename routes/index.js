9e92017e5bf5076ef206c3a7e3851a735d8ffb81
Jarvis Falia <nocodyca@gmail.com>
2025-11-06 01:58:58 -0500

    set up index.js, index.hbs, and added super basic css

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,23 @@ router.get('/', function (req, res, next) {
 
     // read from data.json
     fs.readFile('./data/data.json', 'utf8', function (err, data) {
+        if (err) {
+            console.error(err);
+            return res.render('index', {
+                title: 'Band of the Day',
+                randomItem: 'Error loading data.'
+            });
+        }
+
+        const bands = JSON.parse(data);
+
         // randomly select an item from data
+        const randomIndex = Math.floor(Math.random() * bands.length);
+        randomItem = bands[randomIndex];
 
         // render the index.hbs template with the randomly selected item 
         res.render('index', {
-            title: 'My Random Generator',
+            title: 'Band of the Day',
             randomItem: randomItem,
         });
     });
